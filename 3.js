81f41bbb7d024d4b28771e93f45bd9fdda78f56a
Rudra Pandya <viralpandya079@gmail.com>
2024-12-13 12:57:16 +0530

    till 4.js

--- 3.js
+++ 3.js
@@ -1,11 +1,11 @@
-// function checkLeapYear(year) {
-//     if((0 == year % 4)&&(0 !== year % 100)||(0 == year % 400)){
-//         console.log(`${year} Leap year`);
-//     }
-//     else{
-//         console.log(`${year} Not a leap year`);
-//     }
-// }
-// const year = 2028;
+function checkLeapYear(year) {
+    if((0 == year % 4)&&(0 !== year % 100)||(0 == year % 400)){
+        console.log(`${year} Leap year`);
+    }
+    else{
+        console.log(`${year} Not a leap year`);
+    }
+}
+const year = 2028;
 
-// checkLeapYear(year);
+checkLeapYear(year);
